29debc0962eaceb0001d47a42188b71ed520fa86
WhyTSwag <jeremy.bidet@gmail.com>
2014-03-26 15:25:43 +0100

    Edit sonar-runner.bat

--- sonar-runner.bat
+++ sonar-runner.bat
@@ -1,7 +1,7 @@
 @REM run a sonar analyse of current project granted with admin rights
 @REM START D:\Downloads\Software\SonarQube\sonarqube-4.1.2\sonarqube-4.1.2\bin\windows-x86-64\StartSonar.bat
 sc start SonarQube
-@echo (
+
 D:\Downloads\Software\SonarQube\sonar-runner-dist-2.3\sonar-runner-2.3\bin\sonar-runner.bat -Dsonar.login=admin -Dsonar.password=admin
-) > sonar-runner.log
+
 PAUSE
